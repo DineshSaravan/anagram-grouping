8e88795fd033ff333bbaa13c69b4de92136ec2e1
DineshSaravan <dineshsaravan94@outlook.com>
2021-10-12 02:31:40 +0100

    Use event stream code implementation to fetch file contents

--- anagram/anagram.ts
+++ anagram/anagram.ts
@@ -1,6 +1,6 @@
-// import { readLineByLine } from '../services/file.service';
 import fs from 'fs';
 import readline from 'readline';
+import eventStream from 'event-stream';
 
 /* Anagram groups data
    key - alphabetised word for a anagram group
@@ -32,14 +32,36 @@ export async function readLineByLine(path: string, cb: (line: string) => void) {
     process.exit(1);
   }
 
-  const fileStream = fs.createReadStream(path);
+  // Start - time calculation of file read
+  console.time('File read time:');
 
+  /*
+  * Event-stream method to fetch contents from file
+  * */
+  const fileStream = fs.createReadStream(path);
+  fileStream
+      .pipe(eventStream.split())
+      .pipe(eventStream
+          .mapSync(function(line) {
+            lineCount++;
+            cb(line);
+          }))
+      .on('error', function(err) {
+        console.error('Error while reading file.', err);
+        process.exit(1);
+      })
+      .on('end', function () {
+        onFileReadCloseAction();
+      });
+
+  /*
+  * Readline - code implementation
+  */
+  /*
   const rl = readline.createInterface({
     input: fileStream,
     crlfDelay: Infinity,
   });
-
-  console.time('File read time:');
 
   rl.on('line', function (line) {
     // increment line count on every line read
@@ -50,13 +72,9 @@ export async function readLineByLine(path: string, cb: (line: string) => void) {
   });
 
   rl.on('close', function () {
-    printAnagramGroups();
-
-    console.timeEnd('File read time:');
-    const used = process.memoryUsage().heapUsed / 1024 / 1024;
-    console.log(`Memory used approximately : ${Math.round(used * 100) / 100} MB`);
-    console.log("Total number of lines in file: ", lineCount);
+    onFileReadCloseAction();
   });
+   */
 }
 
 export function wordFormat(word: string): string {
@@ -66,9 +84,23 @@ export function wordFormat(word: string): string {
 }
 
 function printAnagramGroups(): void {
+    console.log("-----ANAGRAM GROUPS-----");
   anagrams.forEach((value: Set<string>) => {
     // printing out in console grouped anagram words
     let anagramGroupText: string = Array.from(value).join(', ');
     console.log(anagramGroupText);
   });
+}
+
+function printStats(): void {
+    // File read performance stats
+    console.timeEnd('File read time:');  // End - time calculation of file read
+    const used = process.memoryUsage().heapUsed / 1024 / 1024;
+    console.log(`Memory used approximately : ${Math.round(used * 100) / 100} MB`);
+    console.log("Total number of lines in file: ", lineCount);
+}
+
+function onFileReadCloseAction(): void {
+  printAnagramGroups();
+  printStats();
 }
